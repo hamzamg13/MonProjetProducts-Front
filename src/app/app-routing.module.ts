b972152e56dfe8030d2a9832c4547af68ac38a11
MGAIDI Hamza <hamzamg13@gmail.com>
2019-01-22 16:39:45 +0100

    Fin de la navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductsListComponent} from './components/products-list/products-list.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {ProductsAddComponent} from './components/products-add/products-add.component';
+import {ProductsDetailComponent} from './components/products-detail/products-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'list', component: ProductsListComponent},
+  {path: 'add', component: ProductsAddComponent},
+  {path: 'detail/:id', component: ProductsDetailComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
