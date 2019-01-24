9cef1b80858c16c912ede8793529cffb95a49dc5
MGAIDI Hamza <hamzamg13@gmail.com>
2019-01-24 20:19:04 +0100

    Fixing my prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,13 @@ export const environment = {
 
   // For spring boot URL
   // URL_Spring_Boot: 'http://localhost:8080/products'
-  URL_Fake: 'http://localhost:8080/products',
+  // URL_Fake: 'http://localhost:8080/products',
+  //
+  // URL_AUTH: 'http://localhost:8080/auth',
 
-  URL_AUTH: 'http://localhost:8080/auth'
+  // For real prod
+  URL_Fake: 'https://still-savannah-18918.herokuapp.com/products',
+
+  URL_AUTH: 'https://still-savannah-18918.herokuapp.com/auth'
+
 };
